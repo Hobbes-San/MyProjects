5dc2dce23f2b8584e773fd538d5db5ac6cdf55ea
Hobbes-San <danchun1125@gmail.com>
2017-10-11 15:07:33 -0700

    Update NearAI_Cpp_Alt.py

--- NearAI_master/NearAI_Cpp_Alt.py
+++ NearAI_master/NearAI_Cpp_Alt.py
@@ -2,9 +2,7 @@ import util
 import time
 import json
 import torch
-from torch import nn
 from torch import optim
-from torch.autograd import Variable
 from pytorch_tools_master.pytorch_tools import torchfold_Alt
 from random import shuffle
 
@@ -56,98 +54,11 @@ for i in range(Cpp_train_size, Cpp_total_size):
         
 shuffle(Cpp_train_data); shuffle(Cpp_test_data)
 
-#The main model.
-class RecursiveModel(nn.Module):
-
-    def __init__(self, vocab_size, size):
-        super(RecursiveModel, self).__init__()
-        self.vocab_size = vocab_size
-        self.size = size
-        self.embedding = nn.Embedding(vocab_size, size)
-        self.out = nn.Linear(size, 2)
-        self.left = nn.Linear(size, 5 * size)
-        self.right = nn.Linear(size, 5 * size)
-        self.from_word = nn.Linear(size, size, bias=False)
-        self.from_children = nn.Linear(size, size, bias=False)
-        self.from_word.weight = nn.Parameter(torch.eye(size))
-        self.from_children.weight = nn.Parameter(torch.eye(size))
-    
-    #The following code is very similar to the forward function of a vanilla LSTM cell.
-    def process_children(self, left_h, left_c, right_h, right_c, word_idx):
-        word_vec = self.embedding(word_idx)
-        lstm_in = self.left(left_h) + self.right(right_h)
-        
-        #Note we now have two forget gates f1 and f2 for left and right children.
-        a, i, f1, f2, o = lstm_in.chunk(5, 1)
-        c = a.tanh() * i.sigmoid() + f1.sigmoid()*left_c + f2.sigmoid()*right_c
-        
-        #We take a combination of the output from the children and the output from
-        #the word vector from the node itself to get the output hidden state.
-        h = self.from_children(o.sigmoid() * c.tanh()) + \
-            self.from_word(word_vec)
-        return h, c
-    
-    def leaf(self, word_idx):
-        return self.embedding(word_idx), Variable(torch.cuda.FloatTensor(word_idx.size()[0], self.size).zero_())
-
-    def logits(self, encoding):
-        return self.out(encoding)
-
-#The following uses torchfold in order to build the computational graphs for the input trees.
-def encode_tree_fold(fold, tree):
-    def encode_node(node):
-        if node.is_leaf():
-            #Split two ways, one for hidden state and one for cell state.
-            return fold.add('leaf', (int(node.val),)).split(2)
-        else:
-            children_states = []
-            for child in node.children:
-                h, c = encode_node(child)
-                children_states += [h, c]
-                
-            #Add the index of the node's word at the end.
-            children_states.append(int(node.val))
-            
-            #Split two ways, one for hidden state and one for cell state.
-            return fold.add('process_children', tuple(children_states)).split(2)
-    encoding, _ = encode_node(tree)
-    return fold.add('logits', (encoding,))
-
-def batch_update_recursive(fold, batch, model, optimizer):
-    all_logits, all_labels = [], []
-    for tree, label in batch:
-        all_logits.append(encode_tree_fold(fold, tree))
-        all_labels.append(label)
-        
-    #Clear grads before updating parameters.
-    optimizer.zero_grad()
-    res = fold.apply(model, all_logits)
-    criterion = nn.CrossEntropyLoss()
-    loss = criterion(res[0], Variable(torch.cuda.LongTensor(all_labels)))
-    loss.backward()
-    optimizer.step()
-    return loss
-
-def validate(fold, batch, model):
-    test_logits, test_labels = [], []
-
-    for tree, label in batch:
-        test_logits.append(encode_tree_fold(fold, tree))
-        test_labels.append(label)
-    res = fold.apply(model, test_logits)
-    
-    #Get prediction by picking the label with highest probability.
-    _, predicted = torch.max(res[0], 1)
-    
-    #Get the number of correct predictions
-    num_correct = (predicted == Variable(torch.cuda.LongTensor(test_labels))).sum().data[0]
-    return num_correct
-
 load_previous = False
 
 #159 is the number of different types of expressions in C++ (i.e. vocabulary size for C++).
 #100 is the embedding dimension.
-Cpp_model = RecursiveModel(159, 100); Cpp_model.cuda()
+Cpp_model = util.RecursiveModel(159, 100); Cpp_model.cuda()
 
 if load_previous:
     print ('Loaded')
@@ -164,13 +75,13 @@ for epoch in range(epochs):
             #Here we create a new fold everytime we process a new batch so that we don't
             #use additional memory by keeping results from old batches around.
             count += 1; Cpp_fold = torchfold_Alt.Fold(); Cpp_fold.volatile = False
-            loss = batch_update_recursive(Cpp_fold, batch, Cpp_model, Cpp_optimizer)
+            loss = util.batch_update_recursive(Cpp_fold, batch, Cpp_model, Cpp_optimizer)
             batch = []
         batch.append((tree, label))
         
     if len(batch) > 0:
         Cpp_fold = torchfold_Alt.Fold(); Cpp_fold.volatile = False
-        batch_update_recursive(Cpp_fold, batch, Cpp_model, Cpp_optimizer)
+        util.batch_update_recursive(Cpp_fold, batch, Cpp_model, Cpp_optimizer)
     
     count_ = 0; batch_ = []; total_correct = 0
     for tree, label in Cpp_test_data:
@@ -179,14 +90,14 @@ for epoch in range(epochs):
             #Here we create a new fold everytime we process a new batch so that we don't
             #use additional memory by keeping results from old batches around.
             count_ += 1; Cpp_fold = torchfold_Alt.Fold(); Cpp_fold.volatile = True
-            num = validate(Cpp_fold, batch_, Cpp_model)
+            num = util.validate(Cpp_fold, batch_, Cpp_model)
             total_correct += num
             batch_ = []
         batch_.append((tree, label))
     
     if len(batch_) > 0:
         Cpp_fold = torchfold_Alt.Fold(); Cpp_fold.volatile = True
-        num = validate(Cpp_fold, batch_, Cpp_model); total_correct += num
+        num = util.validate(Cpp_fold, batch_, Cpp_model); total_correct += num
     
     accuracy = total_correct/len(Cpp_test_data)*100.0
     print (' '.join(['Epoch:', str(epoch), 'Final Accuracy:', str(accuracy), '%']))
