8794fc43ac26239ff6b01c799ded5964b63c6095
Hobbes-San <danchun1125@gmail.com>
2017-10-11 14:19:11 -0700

    Update NearAI_Java_Alt.py

--- NearAI_master/NearAI_Java_Alt.py
+++ NearAI_master/NearAI_Java_Alt.py
@@ -8,292 +8,59 @@ from torch.autograd import Variable
 from pytorch_tools_master.pytorch_tools import torchfold_Alt
 from random import shuffle
 
-idx2word_Cpp = [
-'CursorKind.UNEXPOSED_DECL',
-'CursorKind.STRUCT_DECL',
-'CursorKind.UNION_DECL',
-'CursorKind.CLASS_DECL',
-'CursorKind.ENUM_DECL',
-'CursorKind.FIELD_DECL',
-'CursorKind.ENUM_CONSTANT_DECL',
-'CursorKind.FUNCTION_DECL',
-'CursorKind.VAR_DECL',
-'CursorKind.PARM_DECL',
-'CursorKind.OBJC_INTERFACE_DECL',
-'CursorKind.OBJC_CATEGORY_DECL',
-'CursorKind.OBJC_PROTOCOL_DECL',
-'CursorKind.OBJC_PROPERTY_DECL',
-'CursorKind.OBJC_IVAR_DECL',
-'CursorKind.OBJC_INSTANCE_METHOD_DECL',
-'CursorKind.OBJC_CLASS_METHOD_DECL',
-'CursorKind.OBJC_IMPLEMENTATION_DECL',
-'CursorKind.OBJC_CATEGORY_IMPL_DECL',
-'CursorKind.TYPEDEF_DECL',
-'CursorKind.CXX_METHOD',
-'CursorKind.NAMESPACE',
-'CursorKind.LINKAGE_SPEC',
-'CursorKind.CONSTRUCTOR',
-'CursorKind.DESTRUCTOR',
-'CursorKind.CONVERSION_FUNCTION',
-'CursorKind.TEMPLATE_TYPE_PARAMETER',
-'CursorKind.TEMPLATE_NON_TYPE_PARAMETER',
-'CursorKind.TEMPLATE_TEMPLATE_PARAMETER',
-'CursorKind.FUNCTION_TEMPLATE',
-'CursorKind.CLASS_TEMPLATE',
-'CursorKind.CLASS_TEMPLATE_PARTIAL_SPECIALIZATION',
-'CursorKind.NAMESPACE_ALIAS',
-'CursorKind.USING_DIRECTIVE',
-'CursorKind.USING_DECLARATION',
-'CursorKind.TYPE_ALIAS_DECL',
-'CursorKind.OBJC_SYNTHESIZE_DECL',
-'CursorKind.OBJC_DYNAMIC_DECL',
-'CursorKind.CXX_ACCESS_SPEC_DECL',
-'CursorKind.OBJC_SUPER_CLASS_REF',
-'CursorKind.OBJC_PROTOCOL_REF',
-'CursorKind.OBJC_CLASS_REF',
-'CursorKind.TYPE_REF',
-'CursorKind.CXX_BASE_SPECIFIER',
-'CursorKind.TEMPLATE_REF',
-'CursorKind.NAMESPACE_REF',
-'CursorKind.MEMBER_REF',
-'CursorKind.LABEL_REF',
-'CursorKind.OVERLOADED_DECL_REF',
-'CursorKind.VARIABLE_REF',
-'CursorKind.INVALID_FILE',
-'CursorKind.NO_DECL_FOUND',
-'CursorKind.NOT_IMPLEMENTED',
-'CursorKind.INVALID_CODE',
-'CursorKind.UNEXPOSED_EXPR',
-'CursorKind.DECL_REF_EXPR',
-'CursorKind.MEMBER_REF_EXPR',
-'CursorKind.CALL_EXPR',
-'CursorKind.OBJC_MESSAGE_EXPR',
-'CursorKind.BLOCK_EXPR',
-'CursorKind.INTEGER_LITERAL',
-'CursorKind.FLOATING_LITERAL',
-'CursorKind.IMAGINARY_LITERAL',
-'CursorKind.STRING_LITERAL',
-'CursorKind.CHARACTER_LITERAL',
-'CursorKind.PAREN_EXPR',
-'CursorKind.UNARY_OPERATOR',
-'CursorKind.ARRAY_SUBSCRIPT_EXPR',
-'CursorKind.BINARY_OPERATOR',
-'CursorKind.COMPOUND_ASSIGNMENT_OPERATOR',
-'CursorKind.CONDITIONAL_OPERATOR',
-'CursorKind.CSTYLE_CAST_EXPR',
-'CursorKind.COMPOUND_LITERAL_EXPR',
-'CursorKind.INIT_LIST_EXPR',
-'CursorKind.ADDR_LABEL_EXPR',
-'CursorKind.StmtExpr',
-'CursorKind.GENERIC_SELECTION_EXPR',
-'CursorKind.GNU_NULL_EXPR',
-'CursorKind.CXX_STATIC_CAST_EXPR',
-'CursorKind.CXX_DYNAMIC_CAST_EXPR',
-'CursorKind.CXX_REINTERPRET_CAST_EXPR',
-'CursorKind.CXX_CONST_CAST_EXPR',
-'CursorKind.CXX_FUNCTIONAL_CAST_EXPR',
-'CursorKind.CXX_TYPEID_EXPR',
-'CursorKind.CXX_BOOL_LITERAL_EXPR',
-'CursorKind.CXX_NULL_PTR_LITERAL_EXPR',
-'CursorKind.CXX_THIS_EXPR',
-'CursorKind.CXX_THROW_EXPR',
-'CursorKind.CXX_NEW_EXPR',
-'CursorKind.CXX_DELETE_EXPR',
-'CursorKind.CXX_UNARY_EXPR',
-'CursorKind.OBJC_STRING_LITERAL',
-'CursorKind.OBJC_ENCODE_EXPR',
-'CursorKind.OBJC_SELECTOR_EXPR',
-'CursorKind.OBJC_PROTOCOL_EXPR',
-'CursorKind.OBJC_BRIDGE_CAST_EXPR',
-'CursorKind.PACK_EXPANSION_EXPR',
-'CursorKind.SIZE_OF_PACK_EXPR',
-'CursorKind.LAMBDA_EXPR'
-'CursorKind.OBJ_BOOL_LITERAL_EXPR',
-'CursorKind.OBJ_SELF_EXPR',
-'CursorKind.UNEXPOSED_STMT',
-'CursorKind.LABEL_STMT',
-'CursorKind.COMPOUND_STMT',
-'CursorKind.CASE_STMT',
-'CursorKind.DEFAULT_STMT',
-'CursorKind.IF_STMT',
-'CursorKind.SWITCH_STMT',
-'CursorKind.WHILE_STMT',
-'CursorKind.DO_STMT',
-'CursorKind.FOR_STMT',
-'CursorKind.GOTO_STMT',
-'CursorKind.INDIRECT_GOTO_STMT',
-'CursorKind.CONTINUE_STMT',
-'CursorKind.BREAK_STMT',
-'CursorKind.RETURN_STMT',
-'CursorKind.ASM_STMT',
-'CursorKind.OBJC_AT_TRY_STMT',
-'CursorKind.OBJC_AT_CATCH_STMT',
-'CursorKind.OBJC_AT_FINALLY_STMT',
-'CursorKind.OBJC_AT_THROW_STMT',
-'CursorKind.OBJC_AT_SYNCHRONIZED_STMT',
-'CursorKind.OBJC_AUTORELEASE_POOL_STMT',
-'CursorKind.OBJC_FOR_COLLECTION_STMT',
-'CursorKind.CXX_CATCH_STMT',
-'CursorKind.CXX_TRY_STMT',
-'CursorKind.CXX_FOR_RANGE_STMT',
-'CursorKind.SEH_TRY_STMT',
-'CursorKind.SEH_EXCEPT_STMT',
-'CursorKind.SEH_FINALLY_STMT',
-'CursorKind.MS_ASM_STMT',
-'CursorKind.NULL_STMT',
-'CursorKind.DECL_STMT',
-'CursorKind.TRANSLATION_UNIT',
-'CursorKind.UNEXPOSED_ATTR',
-'CursorKind.IB_ACTION_ATTR',
-'CursorKind.IB_OUTLET_ATTR',
-'CursorKind.IB_OUTLET_COLLECTION_ATTR',
-'CursorKind.CXX_FINAL_ATTR',
-'CursorKind.CXX_OVERRIDE_ATTR',
-'CursorKind.ANNOTATE_ATTR',
-'CursorKind.ASM_LABEL_ATTR',
-'CursorKind.PACKED_ATTR',
-'CursorKind.PURE_ATTR',
-'CursorKind.CONST_ATTR',
-'CursorKind.NODUPLICATE_ATTR',
-'CursorKind.CUDACONSTANT_ATTR',
-'CursorKind.CUDADEVICE_ATTR',
-'CursorKind.CUDAGLOBAL_ATTR',
-'CursorKind.CUDAHOST_ATTR',
-'CursorKind.CUDASHARED_ATTR',
-'CursorKind.VISIBILITY_ATTR',
-'CursorKind.DLLEXPORT_ATTR',
-'CursorKind.DLLIMPORT_ATTR',
-'CursorKind.PREPROCESSING_DIRECTIVE',
-'CursorKind.MACRO_DEFINITION',
-'CursorKind.MACRO_INSTANTIATION',
-'CursorKind.INCLUSION_DIRECTIVE',
-'CursorKind.MODULE_IMPORT_DECL',
-'CursorKind.TYPE_ALIAS_TEMPLATE_DECL'
-]
-
-idx2word_java = [
-'CompilationUnit',
-'Import',
-'Documented',
-'Declaration',
-'TypeDeclaration',
-'PackageDeclaration',
-'ClassDeclaration',
-'EnumDeclaration',
-'InterfaceDeclaration',
-'AnnotationDeclaration',
-'Type',
-'BasicType',
-'ReferenceType',
-'TypeArgument',
-'TypeParameter',
-'Annotation',
-'ElementValuePair',
-'ElementArrayValue',
-'Member',
-'MethodDeclaration',
-'FieldDeclaration',
-'ConstructorDeclaration',
-'ConstantDeclaration',
-'ArrayInitializer',
-'VariableDeclaration',
-'LocalVariableDeclaration',
-'VariableDeclarator',
-'FormalParameter',
-'InferredFormalParameter',
-'Statement',
-'IfStatement',
-'WhileStatement',
-'DoStatement',
-'ForStatement',
-'AssertStatement',
-'BreakStatement',
-'ContinueStatement',
-'ReturnStatement',
-'ThrowStatement',
-'SynchronizedStatement',
-'TryStatement',
-'SwitchStatement',
-'BlockStatement',
-'StatementExpression',
-'TryResource',
-'CatchClause',
-'CatchClauseParameter',
-'SwitchStatementCase',
-'ForControl',
-'EnhancedForControl',
-'Expression',
-'Assignment',
-'TernaryExpression',
-'BinaryOperation',
-'Cast',
-'MethodReference',
-'LambdaExpression',
-'Primary',
-'Literal',
-'This',
-'MemberReference',
-'Invocation',
-'ExplicitConstructorInvocation',
-'SuperConstructorInvocation',
-'MethodInvocation',
-'SuperMethodInvocation',
-'SuperMemberReference',
-'ArraySelector',
-'ClassReference',
-'VoidClassReference',
-'Creator',
-'ArrayCreator',
-'ClassCreator',
-'InnerClassCreator',
-'EnumBody',
-'EnumConstantDeclaration',
-'AnnotationMethod'
-]
-
-idx2word = idx2word_Cpp + idx2word_java
-java_word2idx = {}; Cpp_word2idx = {}
-for i in range(len(idx2word_java)):
-    java_word2idx[idx2word_java[i]] = i
-for i in range(len(idx2word_Cpp)):
-    Cpp_word2idx[idx2word_Cpp[i]] = i
-
+#This program loads the Java trees into the data list, splits it into training
+#and testing sets, then trains for several epochs, measuring the testing accuracy
+#at the end of each epoch.
 print (time.localtime())
 
-java_data = []
+Java_data = []
 
-with open('indexed_trees_labeled.jsonl', 'r') as f:
+#Load the data.
+with open('indexed_trees_labeled_Java.jsonl', 'r') as f:
     line_count = 0
     for line in f:
         d = json.loads(line)
         label = d['hired']
         if label == 2:
             label = 1
-        code_serialized = d['solution']
-        language = d['lang']
-        indexed_tree = util.Node(); indexed_tree.val = code_serialized[0]
-        stack = [indexed_tree]; util.deserialize(code_serialized, stack)
-        if language == 'java':
-            java_data.append((indexed_tree, label))
+        serialized_list = d['solutions']; tree_list = []
+        for code_serialized in serialized_list:
+            indexed_tree = util.Node(); indexed_tree.val = code_serialized[0]
+            stack = [indexed_tree]; util.deserialize(code_serialized, stack)
+            tree_list.append(indexed_tree)
+        Java_data.append((tree_list, label))
         line_count += 1
-        if line_count % 10000 == 0:
+        if line_count % 500 == 0:
             print (line_count)
 
 print ('Done with reading data')
 
 print (time.localtime())
 
-shuffle(java_data)
-java_total_size = len(java_data); java_train_size = int(java_total_size*(4/5))
-java_test_size = java_total_size - java_train_size
+shuffle(Java_data); print ('Data size: ' + str(len(Java_data)))
 
-java_train_data = java_data[:java_train_size]; java_test_data = java_data[java_train_size:]
+#Split into training and testing sets, making sure to keep a single persons's
+#code solutions either all in the training set or all in the testing set to
+#prevent data leakage.
+Java_total_size = len(Java_data); Java_train_size = int(Java_total_size*(4/5))
+Java_test_size = Java_total_size - Java_train_size
+Java_train_data = []; Java_test_data = []
+for i in range(Java_train_size):
+    label = Java_data[i][1]
+    for tree in Java_data[i][0]:
+        Java_train_data.append((tree, label))
+for i in range(Java_train_size, Java_total_size):
+    label = Java_data[i][1]
+    for tree in Java_data[i][0]:        
+        Java_test_data.append((tree, label))
 
+shuffle(Java_train_data); shuffle(Java_test_data)
+
+#The main model.
 class RecursiveModel(nn.Module):
 
     def __init__(self, vocab_size, size):
         super(RecursiveModel, self).__init__()
-        self.volatile = False
         self.vocab_size = vocab_size
         self.size = size
         self.embedding = nn.Embedding(vocab_size, size)
@@ -305,11 +72,17 @@ class RecursiveModel(nn.Module):
         self.from_word.weight = nn.Parameter(torch.eye(size))
         self.from_children.weight = nn.Parameter(torch.eye(size))
         
+    #The following code is very similar to the forward function of a vanilla LSTM cell.
     def process_children(self, left_h, left_c, right_h, right_c, word_idx):
         word_vec = self.embedding(word_idx)
         lstm_in = self.left(left_h) + self.right(right_h)
+        
+        #Note we now have two forget gates f1 and f2 for left and right children.
         a, i, f1, f2, o = lstm_in.chunk(5, 1)
         c = a.tanh() * i.sigmoid() + f1.sigmoid()*left_c + f2.sigmoid()*right_c
+        
+        #We take a combination of the output from the children and the output from
+        #the word vector from the node itself to get the output hidden state.
         h = self.from_children(o.sigmoid() * c.tanh()) + \
             self.from_word(word_vec)
         return h, c
@@ -320,16 +93,22 @@ class RecursiveModel(nn.Module):
     def logits(self, encoding):
         return self.out(encoding)
 
+#The following uses torchfold in order to build the computational graphs for the input trees.
 def encode_tree_fold(fold, tree):
     def encode_node(node):
         if node.is_leaf():
+            #Split two ways, one for hidden state and one for cell state.
             return fold.add('leaf', (int(node.val),)).split(2)
         else:
             children_states = []
             for child in node.children:
                 h, c = encode_node(child)
                 children_states += [h, c]
+            
+            #Add the index of the node's word at the end.
             children_states.append(int(node.val))
+            
+            #Split two ways, one for hidden state and one for cell state.
             return fold.add('process_children', tuple(children_states)).split(2)
     encoding, _ = encode_node(tree)
     return fold.add('logits', (encoding,))
@@ -339,6 +118,8 @@ def batch_update_recursive(fold, batch, model, optimizer):
     for tree, label in batch:
         all_logits.append(encode_tree_fold(fold, tree))
         all_labels.append(label)
+        
+    #Clear grads before updating parameters.
     optimizer.zero_grad()
     res = fold.apply(model, all_logits)
     criterion = nn.CrossEntropyLoss()
@@ -354,26 +135,35 @@ def validate(fold, batch, model):
         test_logits.append(encode_tree_fold(fold, tree))
         test_labels.append(label)
     res = fold.apply(model, test_logits)
+    
+    #Get prediction by picking the label with highest probability.
     _, predicted = torch.max(res[0], 1)
+    
+    #Get the number of correct predictions
     num_correct = (predicted == Variable(torch.cuda.LongTensor(test_labels))).sum().data[0]
     return num_correct
 
 load_previous = False
-java_model = RecursiveModel(len(java_word2idx), 50); java_model.cuda()
+
+#77 is the number of different types of expressions in Java (i.e. vocabulary size for Java).
+#50 is the embedding dimension.
+java_model = RecursiveModel(77, 50); java_model.cuda()
 
 if load_previous:
     print ('Loaded')
     java_model.load_state_dict(torch.load('java_checkpoint_Alt.pth'))
 java_optimizer = optim.Adagrad(java_model.parameters(), lr=0.01, weight_decay=0.001)
 
-epochs = 10; prev_accuracy = 55
+epochs = 10; prev_accuracy = 50
 for epoch in range(epochs):
-    shuffle (java_train_data)
+    shuffle (Java_train_data)
     batch = []; count = 0
-    for tree, label in java_train_data:
+    for tree, label in Java_train_data:
         if len(batch) >= 50:
-            count += 1
-            java_fold = torchfold_Alt.Fold(); java_fold.volatile = False
+            
+            #Here we create a new fold everytime we process a new batch so that we don't
+            #use additional memory by keeping results from old batches around.
+            count += 1; java_fold = torchfold_Alt.Fold(); java_fold.volatile = False
             loss = batch_update_recursive(java_fold, batch, java_model, java_optimizer)
             batch = []
         batch.append((tree, label))
@@ -383,8 +173,11 @@ for epoch in range(epochs):
         batch_update_recursive(java_fold, batch, java_model, java_optimizer)
     
     count_ = 0; batch_ = []; total_correct = 0
-    for tree, label in java_test_data:
+    for tree, label in Java_test_data:
         if len(batch_) >= 50:
+            
+            #Here we create a new fold everytime we process a new batch so that we don't
+            #use additional memory by keeping results from old batches around.
             count_ += 1; java_fold = torchfold_Alt.Fold(); java_fold.volatile = True
             num = validate(java_fold, batch_, java_model)
             total_correct += num
@@ -395,17 +188,19 @@ for epoch in range(epochs):
         java_fold = torchfold_Alt.Fold(); java_fold.volatile = True
         num = validate(java_fold, batch_, java_model); total_correct += num
     
-    accuracy = total_correct/len(java_test_data)*100.0
+    accuracy = total_correct/len(Java_test_data)*100.0
     print (' '.join(['Epoch:', str(epoch), 'Final Accuracy:', str(accuracy), '%']))
     print (time.localtime())
     
+    #If the accuracy is better than the best accuracy so far, write down the training
+    #and testing data as JSON files and save the parameters of the model.
     if accuracy > prev_accuracy:
         with open('indexed_trees_labeled_Java_Train_Alt.jsonl', 'w') as f:
-            for tree, label in java_train_data:
+            for tree, label in Java_train_data:
                 r = []; util.serialize(tree, r)
                 f.write(json.dumps({'hired': label, 'solution': r, 'language': 'Java'}) + '\n')
         with open('indexed_trees_labeled_Java_Test_Alt.jsonl', 'w') as f:
-            for tree, label in java_test_data:
+            for tree, label in Java_test_data:
                 r = []; util.serialize(tree, r)
                 f.write(json.dumps({'hired': label, 'solution': r, 'language': 'Java'}) + '\n')
         torch.save(java_model.state_dict(), 'java_checkpoint_Alt.pth')
