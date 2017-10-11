639a3e29f22e7e7abe63db04c1335ff3b9ab0486
Hobbes-San <danchun1125@gmail.com>
2017-10-11 15:14:34 -0700

    Update testJava.py

--- NearAI_master/testJava.py
+++ NearAI_master/testJava.py
@@ -3,7 +3,6 @@ import time
 import json
 import torch
 from pytorch_tools_master.pytorch_tools import torchfold_Alt
-import NearAI_Java_Alt
 
 #This is a testing program for Java code classification.
 print (time.localtime())
@@ -30,7 +29,7 @@ print ('Done with reading data')
 print (time.localtime())
 
 #Load the saved parameters.
-java_model = NearAI_Java_Alt.RecursiveModel(77, 50); java_model.cuda()
+java_model = util.RecursiveModel(77, 50); java_model.cuda()
 java_model.load_state_dict(torch.load('java_checkpoint_Alt.pth'))
 
 epochs = 1
@@ -39,14 +38,14 @@ for epoch in range(epochs):
     for tree, label in java_test_data:
         if len(batch_) >= 50:
             count_ += 1; java_fold = torchfold_Alt.Fold(); java_fold.volatile = True
-            num = NearAI_Java_Alt.validate(java_fold, batch_, java_model)
+            num = util.validate(java_fold, batch_, java_model)
             total_correct += num
             batch_ = []
         batch_.append((tree, label))
     
     if len(batch_) > 0:
         java_fold = torchfold_Alt.Fold(); java_fold.volatile = True
-        num = NearAI_Java_Alt.validate(java_fold, batch_, java_model); total_correct += num
+        num = util.validate(java_fold, batch_, java_model); total_correct += num
     
     accuracy = total_correct/len(java_test_data)*100.0
     print (' '.join(['Epoch:', str(epoch), 'Final Accuracy:', str(accuracy), '%']))
